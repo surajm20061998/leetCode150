f84052b1003c8e9e0f77af4b0e9e22368d2e7d77
surajm20061998 <surajm20061998@gmail.com>
2026-01-29 14:38:43 -0500

    Rotating array to the right by k steps

--- rotateArray.py
+++ rotateArray.py
@@ -12,3 +12,27 @@ class Solution:
 
 #A more optimized solution??
 
+#reverse entire array
+#reverse first k elements
+#reverse the remaining n-k elements
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        #reverse entire array
+        #reverse first k elements
+        #reverse the remaining n-k elements
+
+        def rev(nums, l , r):
+            while(l<r):
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
+                r-=1
+        
+        k%=len(nums)
+        rev(nums, 0, len(nums)-1)
+        rev(nums, 0, k-1)
+        rev(nums, k, len(nums)-1)
