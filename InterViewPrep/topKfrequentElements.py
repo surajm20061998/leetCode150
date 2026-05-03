773808d52aa2c6155c78c6f0ded23f0120094d42
surajm20061998 <surajm20061998@gmail.com>
2026-05-03 17:52:25 -0400

    Interview Prep | Top K frequent elements

---
+++ InterViewPrep/topKfrequentElements.py
@@ -0,0 +1,9 @@
+# Optimal Solution - Not Sure
+# Time Complexity - O(nlogk)
+# Space Complexity - O(n)
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        return [i[0] for i in counter.most_common(k)]
+        
