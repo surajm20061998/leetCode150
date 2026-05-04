d66ca729abd02c57b22b0a931fe9d7c14c27bce9
surajm20061998 <surajm20061998@gmail.com>
2026-05-04 17:40:57 -0400

    Interview Prep | Lowest Common Ancestor of Binary Tree

---
+++ InterViewPrep/lowestCommonAncestorOfBT.py
@@ -0,0 +1,28 @@
+# Optimal Solution
+# Time Complexity - O(n)
+# Space Complexity - O(h)
+
+# Need to study solution again
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        if not root or root==p or root==q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+
+        if left and right:
+            return root
+        
+        return left or right
+
+        
