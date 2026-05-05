62b2b8c964893de0bdf654ca9dfa7d5fb9f5b70a
surajm20061998 <surajm20061998@gmail.com>
2026-05-04 20:55:32 -0400

    Interview Prep | Diameter of Binary Tree

--- InterViewPrep/diameterOfBinaryTree.py
+++ InterViewPrep/diameterOfBinaryTree.py
@@ -22,3 +22,30 @@ class Solution:
         
         maxDepth(root)
         return ans
+    
+    
+    
+# Another Solution - 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        res = [0]
+
+        def dfs(node):
+            if not node:
+                return -1
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+            res[0] = max(res[0], 2+left+right)
+
+            return 1+max(left,right)
+        dfs(root)
+        print(res)
+        return res[0]
