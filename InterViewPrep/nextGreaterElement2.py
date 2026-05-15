ae5a9d102bdf24126b60bdc5eda791c6cd12b7dc
surajm20061998 <surajm20061998@gmail.com>
2026-05-15 02:11:47 -0400

    Interview Prep | Next Greater Element 2

---
+++ InterViewPrep/nextGreaterElement2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [-1] * n
+        stack = []  # stores indices
+
+        for i in range(2 * n):
+            curr = nums[i % n]
+
+            while stack and curr > nums[stack[-1]]:
+                idx = stack.pop()
+                res[idx] = curr
+
+            if i < n:
+                stack.append(i)
+
+        return res
