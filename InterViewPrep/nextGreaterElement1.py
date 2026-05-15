f10999ef20f4ef0cea0cdb3c6d0bb44701f80d14
surajm20061998 <surajm20061998@gmail.com>
2026-05-14 21:41:31 -0400

    Interview Prep | Next Greater Element 1 Optimal solution

--- InterViewPrep/nextGreaterElement1.py
+++ InterViewPrep/nextGreaterElement1.py
@@ -1,3 +1,23 @@
+# Optimal Solution
+# Using a monotonic decreasing stack
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1Idx = {n:i for i,n in enumerate(nums1)}
+        res = [-1]*len(nums1)
+        stack=[]
+
+        for i in range(len(nums2)):
+            curr = nums2[i]
+            while stack and curr>stack[-1]:
+                val = stack.pop() # curr is the next greates element of val or of stack.top()
+                idx = nums1Idx[val]
+                res[idx] = curr
+
+            if curr in nums1Idx: # base condition of adding elemet to stack
+                stack.append(curr)
+        return res
+
+
 #Not optimal, need to see solution with monotonic stack
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
