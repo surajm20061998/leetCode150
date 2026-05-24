bf681a51a18c332f52e95df1023cdb0937326c1b
surajm20061998 <surajm20061998@gmail.com>
2026-05-24 18:36:31 -0400

    Interview Prep | Trapping Rain Water Optimal Solution

--- InterViewPrep/trappingRainWater.py
+++ InterViewPrep/trappingRainWater.py
@@ -24,3 +24,28 @@ class Solution:
 
         return sum(trap)
         
+# Optimal Solution using Two Pointers
+# Linear time and space
+# Need to understand again
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        if not height:
+            return 0
+
+        l,r = 0, len(height)-1
+        leftMax, rightMax = height[l], height[r]
+        result = 0
+
+        while l<r:
+            if leftMax < rightMax:
+                l+=1
+                leftMax = max(leftMax, height[l])
+                result += leftMax - height[l]
+            else:
+                r-=1
+                rightMax = max(rightMax, height[r])
+                result += rightMax - height[r]
+        
+        return result
