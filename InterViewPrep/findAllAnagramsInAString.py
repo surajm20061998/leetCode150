57f3ba62f81a5285405db26800a1e7514ace3c3c
surajm20061998 <surajm20061998@gmail.com>
2026-06-05 04:51:26 -0400

    Interview Prep | Find All Anagrams of a String

--- InterViewPrep/findAllAnagramsInAString.py
+++ InterViewPrep/findAllAnagramsInAString.py
@@ -10,3 +10,33 @@ class Solution:
             if (''.join(sorted(s[i:i+n])) == p):
                 output.append(i)
         return output
+
+# Better or Optimal Solution 
+# CharSetSize = 26
+# Time Complexity = O(n*CharSetSize) 
+# Space Complexity = O(charSetSize)
+from collections import Counter
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(p) > len(s) :
+            return []
+        
+        ans = []
+        need = Counter(p)
+        window = Counter()
+        winLen = len(p)
+        start = 0
+
+        for end in range(len(s)):
+            window[s[end]] += 1
+
+            if (end - start + 1) > winLen:
+                window[s[start]] -= 1
+                if window[s[start]] == 0:
+                    del window[s[start]]
+                start+=1
+            if window == need:
+                ans.append(start)
+        return ans
+
+        
