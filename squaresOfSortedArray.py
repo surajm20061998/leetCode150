382ee42f67f0af1b623050a953d563e306e6bede
surajm20061998 <surajm20061998@gmail.com>
2026-06-06 04:14:15 -0400

    Interview Prep | Squares of Sorted Arrays

--- squaresOfSortedArray.py
+++ squaresOfSortedArray.py
@@ -17,3 +17,19 @@ class Solution:
                 right-=1
         return res
         
+# Attempted Again : 
+# Similar Intuition :
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans = []
+        l,r=0,len(nums)-1
+
+        while(l<=r):
+            if (nums[l])**2 > (nums[r]**2):
+                ans.append((nums[l])**2)
+                l+=1
+            else:
+                ans.append(nums[r]**2)
+                r-=1
+        return ans[::-1]
+        
