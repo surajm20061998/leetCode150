e1972294b484c3cacf5269258f19183d4888fe97
surajm20061998 <surajm20061998@gmail.com>
2026-06-05 20:28:46 -0400

    Interview Prep | Remove Element

--- removeElement.py
+++ removeElement.py
@@ -1,12 +1,17 @@
+# Two Pointer
+# l points to the next position where a valid element should be placed.
+# r scans every element.
+# If nums[r] != val, copy it to nums[l] and move l forward.
+# At the end, l is the number of elements not equal to val.
+# Time Complexity = O(n)
+# Space Complexity = O(1)
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        i = 0
-
-        for num in nums:
-            if num!=val:
-                nums[i] = num
-                i+=1
-        
-        return i
+        l=0
+        for r in range(len(nums)):
+            if nums[r]!=val:
+                nums[l] = nums[r]
+                l+=1
+        return l
 
         
