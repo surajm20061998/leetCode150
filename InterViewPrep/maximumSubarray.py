be5c3016d13f4b7e306633f279736b8e9e6f88ad
surajm20061998 <surajm20061998@gmail.com>
2026-07-09 14:21:07 -0400

    Maximum Subarray

---
+++ InterViewPrep/maximumSubarray.py
@@ -0,0 +1,14 @@
+# Sliding Window
+# Remove negative prefix
+# Need to check Agaim
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]
+        currSum = 0
+
+        for n in nums:
+            if currSum < 0:
+                currSum=0
+            currSum += n
+            maxSub = max(maxSub, currSum)
+        return maxSub
